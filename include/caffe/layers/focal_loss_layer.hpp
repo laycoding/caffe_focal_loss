7151863ec3ad4d87a1fdf02e95cbb264d93457cb
Weidong Zhang <battle975444818@outlook.com>
2018-08-07 15:17:25 +0800

    focal loss

---
+++ include/caffe/layers/focal_loss_layer.hpp
@@ -0,0 +1,55 @@
+#ifndef CAFFE_FOCAL_LOSS_LAYER_HPP_
+#define CAFFE_FOCAL_LOSS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/loss_layer.hpp"
+
+namespace caffe
+{
+template <typename Dtype>
+class FocalLossLayer : public LossLayer<Dtype>
+{
+  public:
+    explicit FocalLossLayer(const LayerParameter &param)
+        : LossLayer<Dtype>(param) {}
+    virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                            const vector<Blob<Dtype> *> &top);
+    virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
+                         const vector<Blob<Dtype> *> &top);
+
+    virtual inline const char *type() const { return "FocalLoss"; }
+    virtual inline int ExactNumBottomBlobs() const { return -1; }
+    virtual inline int MinBottomBlobs() const { return 2; }
+    virtual inline int MaxBottomBlobs() const { return 3; }
+    virtual inline int ExactNumTopBlobs() const { return -1; }
+    virtual inline int MinTopBlobs() const { return 1; }
+    virtual inline int MaxTopBlobs() const { return 2; }
+
+  protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                             const vector<Blob<Dtype> *> &top);
+    // virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
+    //                          const vector<Blob<Dtype> *> &top);
+
+    virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
+                              const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+    // virtual void Backward_gpu(const vector<Blob<Dtype> *> &top,
+    //                           const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom);
+
+
+
+    int batch_size, cls_cnt;
+
+    Blob<Dtype> scaler_;
+    vector<Dtype> alphas_;
+    Dtype gamma_;
+};
+
+} // namespace caffe
+
+#endif
---
+++ src/caffe/layers/focal_loss_layer.cpp
@@ -0,0 +1,81 @@
+#include "caffe/layers/focal_loss_layer.hpp"
+#include "caffe/util/math_function.hpp"
+
+namespace caffe
+{
+
+template <typename Dtype>
+void FocalLossLayer<Dtype>::LayerSetUp(
+    const vector<Blob<Dtype> *> &bottom, const vector<Blob<Dtype> *> &top)
+{
+    LossLayer<Dtype>::LayerSetUp(bottom, top);
+    // alphas_ = this->layer_param().focal_loss_param().alpha();
+    const FocalLossParameter& focal_param = this->layer_param_.focal_loss_param();
+    alphas_.clear();
+    std::copy(.alpha().begin(),
+        focal_param.slice_point().end(),
+        std::back_inserter(alphas_));
+
+    gamma_ = focal_param.gamma();
+    batch_size = bottom[0]->count(0)
+    cls_cnt = bottom[0]->count(1)
+}
+
+template <typename Dtype>
+void FocalLossLayer<Dtype>::Reshape(
+    const vector<Blob<Dtype> *> &bottom, const vector<Blob<Dtype> *> &top)
+{
+    top[0].Reshape(1);
+}
+
+template <typename Dtype>
+void FocalLossLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype> *> &bottom, const vector<Blob<Dtype> *> &top)
+{
+    const Dtype *prob_data = bottom[0]->cpu_data();
+    const Dtype *label = bottom[1]->cpu_data();
+    Dtype loss = 0;
+
+    for (int i = 0; i < batch_size; ++i)
+    {
+        const int label_value = static_cast<int>(label[i]);
+        const Dtype prob = prob_data[i*cls_cnt + label_value];
+        loss += -alphas[label_value] * powf((1 - prob), gamma_) * log(prob) / batch_size;
+    }
+    top[0]->mutable_cpu_data()[0] = loss;
+}
+
+template <typename Dtype>
+void FocalLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype> *> &top,
+                                                    const vector<bool> &propagate_down, const vector<Blob<Dtype> *> &bottom)
+{
+    if (propagate_down[1])
+    {
+        LOG(FATAL) << this->type()
+                   << " Layer cannot backpropagate to label inputs.";
+    }
+    if (propagate_down[0])
+    {
+        Dtype *bottom_diff = bottom[0]->mutable_cpu_diff();
+        const Dtype *prob_data = bottom[0]->cpu_data();
+        const Dtype *label = bottom[1]->cpu_data();
+
+        caffe_set(batch_size * cls_cnt, 0, bottom_diff);
+
+        for (int i = 0; i < batch_size; ++i)
+        {
+            const int label_value = static_cast<int>(label[i]);
+            const Dtype prob = prob_data[i*cls_cnt + label_value];
+            bottom_diff[i * cls_cnt + label_value] = -alphas_[label_value] * (-gamma_ * powf((1-prob), gamma_ - 1) * log(prob) 
+                                                    + powf((1-prob), gamma_) / prob);
+        }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(FocalLossLayer);
+#endif
+
+INSTANTIATE_CLASS(FocalLossLayer);
+REGISTER_LAYER_CLASS(FocalLoss);
+
+} // namespace caffe
